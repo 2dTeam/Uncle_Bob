ad033f8925d464ad69720cb92a3e933327eb8311
VitalyNikolaev <nikolaev.vitally@yandex.ru>
2016-11-03 06:09:50 +0300

    bump

--- app/src/main/java/org/team2d/uncle_bob/MainActivity.java
+++ app/src/main/java/org/team2d/uncle_bob/MainActivity.java
@@ -8,30 +8,26 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
-import android.support.v7.app.AlertDialog;
-import android.util.Log;
-import android.view.View;
-import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ArrayAdapter;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
-import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
-
-import org.team2d.uncle_bob.Database.DatabaseAccess;
+import org.slf4j.LoggerFactory;
 import org.team2d.uncle_bob.Database.DatabaseService;
 import org.team2d.uncle_bob.Database.ORM.PizzaORM;
 
@@ -43,11 +39,11 @@ public class MainActivity extends AppCompatActivity
 
     private static final String TAG = MainActivity.class.getSimpleName();
     private static final Logger LOGGER = LoggerFactory.getLogger(MainActivity.class);
-    private final int  PERMISSION_REQUEST_PHONE_CODE = 1;
+    private final int PERMISSION_REQUEST_PHONE_CODE = 1;
+    HashMap<Integer, PizzaORM> pizza  = DatabaseService.getPizza(this);
 
 
-    public  int getResourceId(String pVariableName, String pResourcename, String pPackageName)
-    {
+    public int getResourceId(String pVariableName, String pResourcename, String pPackageName) {
         try {
             return getResources().getIdentifier(pVariableName, pResourcename, pPackageName);
         } catch (Exception e) {
@@ -86,7 +82,6 @@ public class MainActivity extends AppCompatActivity
         // load from db pizzas
         LinearLayout linearLayout1 = (LinearLayout) findViewById(R.id.linearLayout1);
 
-        HashMap<Integer, PizzaORM> pizza  = DatabaseService.getPizza(this);
 
         for (HashMap.Entry<Integer, PizzaORM> entry : pizza.entrySet()) {
             int key = entry.getKey();
@@ -108,8 +103,6 @@ public class MainActivity extends AppCompatActivity
 
             lp.addRule(RelativeLayout.CENTER_HORIZONTAL);
             lp.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
-
-
 
             tv.setLayoutParams(lp);
 
