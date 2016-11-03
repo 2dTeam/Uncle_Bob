f9fcfb1437caac517d5542047bc064e146251d43
VitalyNikolaev <nikolaev.vitally@yandex.ru>
2016-11-03 15:12:12 +0300

    basket activity added

--- app/src/main/java/org/team2d/uncle_bob/MainActivity.java
+++ app/src/main/java/org/team2d/uncle_bob/MainActivity.java
@@ -201,20 +201,13 @@ public class MainActivity extends AppCompatActivity
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
+        switch (id) {
+            case R.id.nav_basket : {
+                Intent intent = new Intent(this, BasketActivity.class);
+                startActivity(intent);
+            }
+        }
 
-//        if (id == R.id.nav_camera) {
-//            // Handle the camera action
-//        } else if (id == R.id.nav_gallery) {
-//
-//        } else if (id == R.id.nav_slideshow) {
-//
-//        } else if (id == R.id.nav_manage) {
-//
-//        } else if (id == R.id.nav_share) {
-//
-//        } else if (id == R.id.nav_send) {
-//
-//        }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
