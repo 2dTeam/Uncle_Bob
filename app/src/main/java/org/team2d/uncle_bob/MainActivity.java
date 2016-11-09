973a5509dd2c43a2b8891f6a53639c4736d0c92c
pavelrpaul <pavel.rukavishnikov@gmail.com>
2016-11-09 03:19:39 +0300

    add okhttp. noService
    add okhttp. noService

--- app/src/main/java/org/team2d/uncle_bob/MainActivity.java
+++ app/src/main/java/org/team2d/uncle_bob/MainActivity.java
@@ -23,6 +23,12 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
+import okhttp3.OkHttpClient;
+import java.io.IOException;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
