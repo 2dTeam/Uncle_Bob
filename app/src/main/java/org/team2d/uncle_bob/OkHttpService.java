f930677ae97313ffb0755a95b6b367ffaf701b1f
pavelrpaul <pavel.rukavishnikov@gmail.com>
2016-11-11 01:33:17 +0300

    addService
    serv

---
+++ app/src/main/java/org/team2d/uncle_bob/OkHttpService.java
@@ -0,0 +1,46 @@
+package org.team2d.uncle_bob;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
+
+
+
+import java.io.File;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+/**
+ * Created by pavelrukavishnikov on 11.11.16.
+ */
+
+public class OkHttpService extends IntentService {
+
+    public OkHttpService() {
+        super("OkHttpService");
+
+    }
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        //Example implementation
+        String url = new String("");
+
+        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+        builder.connectTimeout(10, TimeUnit.SECONDS);
+        builder.readTimeout(10, TimeUnit.SECONDS);
+
+
+        Request.Builder requestBuilder = new Request.Builder().url(url);
+
+        //check data and call post
+
+    }
+}
