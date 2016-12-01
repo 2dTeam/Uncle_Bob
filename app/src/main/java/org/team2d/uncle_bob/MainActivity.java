17870d67e94e86339257079bdc74673ce7b8759f
VitalyNikolaev <nikolaev.vitally@yandex.ru>
2016-12-01 03:48:54 +0300

    Network half ready

--- app/src/main/java/org/team2d/uncle_bob/MainActivity.java
+++ app/src/main/java/org/team2d/uncle_bob/MainActivity.java
@@ -9,7 +9,6 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
@@ -19,6 +18,7 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -26,6 +26,13 @@ import android.view.View;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.team2d.uncle_bob.Basket.Basket;
+import org.team2d.uncle_bob.Network.Network;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
 
 
 public class MainActivity extends AppCompatActivity
@@ -49,6 +56,19 @@ public class MainActivity extends AppCompatActivity
             // read saved basket from storage
         }
 
+        Network.sendOrderToServer(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                Log.d("Network", "Response: " + response);
+            }
+        });
+
+        Network.sendOrderToServer(null);
         setupFAB();
         setupDrawer();
     }
---
+++ app/src/main/java/org/team2d/uncle_bob/Network/Network.java
@@ -0,0 +1,47 @@
+package org.team2d.uncle_bob.Network;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.RequestBody;
+
+/**
+ * Created by nikolaev on 17.11.16.
+ */
+
+public class Network {
+    private static OkHttpClient client = new OkHttpClient();
+    private static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+
+    private static void makePostRequest(String url, RequestBody requestBody, Callback callback) throws IOException {
+        okhttp3.Request request = new okhttp3.Request.Builder()
+                .url(url)
+                .post(requestBody)
+                .addHeader("content-type", "application/json; charset=utf-8")
+                .build();
+
+        client.newCall(request).enqueue(callback);
+    }
+
+
+    public static void sendOrderToServer(Callback callback) {
+        JSONObject mockJson = new JSONObject();
+        try {
+            mockJson.put("username", "Николаев Виталий");
+        } catch (JSONException e){
+            e.printStackTrace();
+        }
+        RequestBody json = RequestBody.create(JSON, mockJson.toString());
+        try {
+            makePostRequest("http://unclebob.ml/order", json, callback);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
